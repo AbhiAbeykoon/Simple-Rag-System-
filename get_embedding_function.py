b7876f415f1b329110e3ade7c8d6c2e0e54009f3
AbhiAbeykoon <127072542+AbhiAbeykoon@users.noreply.github.com>
2025-11-30 07:52:51 +0530

    Change API key placeholder in embedding function
    Updated GOOGLE_API_KEY to prompt for user input.

--- get_embedding_function.py
+++ get_embedding_function.py
@@ -1,10 +1,11 @@
 from langchain_google_genai import GoogleGenerativeAIEmbeddings
 
-GOOGLE_API_KEY = "AIzaSyAPgpFXiPfrClTfoSWsVYRc62M66SzBhZ0"
+GOOGLE_API_KEY = "Enter Your API key"
 
 def get_embedding_function():
     embeddings = GoogleGenerativeAIEmbeddings(
         model="models/text-embedding-004", 
         google_api_key=GOOGLE_API_KEY
     )
+
     return embeddings
