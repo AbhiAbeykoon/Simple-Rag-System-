8a17313a12af4797ecf5d072f507ab8c510b4c46
AbhiAbeykoon <127072542+AbhiAbeykoon@users.noreply.github.com>
2025-11-30 07:52:21 +0530

    Remove unused code from check.py

--- check.py
+++ check.py
@@ -1,9 +1,2 @@
-from langchain_ollama import OllamaLLM
 
-model = OllamaLLM(model="mistral")
-response = model.invoke("Hello, how are you?")
 
-if response:
-    print(response)
-else:
-    print("No response")
